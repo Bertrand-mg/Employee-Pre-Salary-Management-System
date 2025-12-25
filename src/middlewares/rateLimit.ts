1521984a911121da0b51642f4a983feeb38cb387
Bertrand-mg <bertrandmutabazig@gmail.com>
2025-12-25 02:37:10 +0200

    Configured cors, helmet, rateLimit

---
+++ src/middlewares/rateLimit.ts
@@ -0,0 +1,12 @@
+import { Express } from 'express';
+import rateLimit from 'express-rate-limit';
+
+export const applyLimit = (app: Express) => {
+  const apilimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 2,
+    standardHeaders: true,
+    legacyHeaders: false,
+  });
+  app.use('/api', apilimiter);
+};
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import express, { Express } from 'express';
+import helmet from 'helmet';
+import cors from 'cors';
 import { config as Dotenv } from 'dotenv';
 Dotenv();
 import { configs } from './config';
 import mainRoute from './routes';
 import { connectToDatabase } from './database';
+import { applyLimit } from './middlewares';
 
 const app: Express = express();
 
@@ -11,6 +14,9 @@ app.use(configs.prefix, mainRoute);
 
 const startApp = async () => {
   try {
+    app.use(helmet());
+    applyLimit(app);
+    app.use(cors());
     app.use(express.json());
 
     await connectToDatabase();
